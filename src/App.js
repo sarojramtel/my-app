5b6e9319a79dd6cc0956fc35caca35ecbabd9fc2
sarojramtel <nepalisaroj056@gmail.com>
2022-12-21 18:14:48 +0545

    Prepared a product list

--- src/App.js
+++ src/App.js
@@ -5,8 +5,13 @@ import { STOCKS } from "./constants";
 import Counter from "./Counter";
 import { useState } from "react";
 import { BsPlusCircle } from "react-icons/bs";
+import ProductsList from "./components/productsList";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <ProductsList />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/productsList.js
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import { BsTrash } from "react-icons/bs";
+import { BiEdit } from "react-icons/bi";
+
+const ProductsList = () => {
+  const [products, setProducts] = useState([]);
+  const [name, setName] = useState("");
+  const [brand, setBrand] = useState("");
+  const [price, setPrice] = useState("");
+
+  const addProduct = () =>
+    setProducts([
+      ...products,
+      {
+        name,
+        brand,
+        price,
+        id: new Date().getTime(),
+      },
+    ]);
+
+  const removeProduct = (selectedId) =>
+    setProducts(products.filter((p) => p.id !== selectedId));
+
+  const handleEditProduct = (product) => {
+    setName(product.name);
+    setBrand(product.brand);
+    setPrice(product.price);
+  };
+
+  return (
+    <div>
+      <h1>Products List</h1>
+      <div className="products-container">
+        {products.map((p, j) => (
+          <div className="product" key={p.id}>
+            <div className="sn">
+              <span>{j + 1}</span>
+            </div>
+            <div className="col">
+              <span>{p.name}</span>
+            </div>
+            <div className="col">
+              <span>{p.brand}</span>
+            </div>
+            <div className="col">
+              <span>{p.price}</span>
+            </div>
+            <div className="col">
+              <BsTrash color={"red"} onClick={() => removeProduct(p.id)} />
+              <BiEdit color={"blue"} onClick={() => handleEditProduct(p)} />
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="product">
+        <input
+          name="name"
+          id="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Product Name"
+        />
+        <input
+          name="brand"
+          id="brand"
+          value={brand}
+          onChange={(e) => setBrand(e.target.value)}
+          placeholder="Brand"
+        />
+        <input
+          name="price"
+          id="price"
+          value={price}
+          onChange={(e) => setPrice(e.target.value)}
+          placeholder="Price"
+          type="number"
+        />
+        <button className="" onClick={addProduct}>
+          Add
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ProductsList;
